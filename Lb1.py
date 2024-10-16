3faced91f99b8f292da9adbdfe76a2ca35b0ee0a
SystemX <dmytro.nechypuruk@nure.ua>
2024-10-16 12:01:28 +0300

    New file was added to the repo

--- Lb1.py
+++ Lb1.py
@@ -1,5 +1,15 @@
 import requests
+import json
 
 url_string = "https://bank.gov.ua/NBU_Exchange/exchange_site?json&start=20241007&end=20241011&valcode=usd"
 
-requests.get(url_string)
+my_response = requests.get(url_string)
+
+print(my_response)
+print(my_response.content)
+
+response_json = json.loads(my_response.content)
+
+print(response_json)
+for item in response_json:
+     print(item['rate'])
